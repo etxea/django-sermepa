b6de05541987c145e72b591c50f668aff3281710
Jon Latorre <jlatorre@irontec.com>
2026-04-12 13:59:10 +0200

    Renombrar el paquete a "django-sermepa-etxea" en pyproject.toml y setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='django-sermepa',
+    name='django-sermepa-etxea',
     version='2.0.0',
     packages=find_packages(include=['sermepa', 'sermepa.*']),
     include_package_data=True,
